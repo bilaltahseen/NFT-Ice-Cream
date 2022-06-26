45c76a089aafde8cdcb4767a7b479abb13ae4333
Bilal <bilal.tehseen@avanzainnovations.com>
2022-06-26 21:19:58 +0500

    Added URL

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <MoralisProvider serverUrl="" appId="">
+     <MoralisProvider serverUrl="https://wrgbueztprwi.usemoralis.com:2053/server" appId="hWWCUNFTtA5Ir3Nou4bqzTf4Vd946zMZRSiedXko">
       <App />
     </MoralisProvider>
   </React.StrictMode>
